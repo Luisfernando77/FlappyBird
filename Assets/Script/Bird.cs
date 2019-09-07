0b5de81578499c99fa1b1fa96cf3f829e6489d55
Luis Fernando <shaldon-fer@hotmail.com>
2019-09-07 11:39:40 -0500

    'flappy'

---
+++ Assets/Script/Bird.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bird : MonoBehaviour
+{
+
+    private bool isDead = false;
+    private Rigidbody2D rb2d;
+    private Animator anim;
+    public float upForce = 50f;
+
+
+    private void Awake()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+
+
+    }
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+
+        if (isDead) return;
+
+        if (Input.GetMouseButton(0))
+        {
+            rb2d.velocity = Vector2.zero;
+            rb2d.AddForce(Vector2.up * upForce);
+            anim.SetTrigger("Flaps");
+            SoundSystem.instance.PlayJump3();
+
+        }
+
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+
+        isDead = true;
+        anim.SetTrigger("Die");
+        //gameController.BirdDie();
+        Invoke("BirdDie", 1f);
+        rb2d.velocity = Vector2.zero;
+        SoundSystem.instance.PlayDead();
+
+    }
+
+    private void BirdDie()
+    {
+        GameController.instance.BirdDie();
+    }
+
+}
---
+++ Assets/Script/GameController.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class GameController : MonoBehaviour
+{
+
+    public static GameController instance;
+
+    public GameObject gamerOverText;
+    public bool gameOver;
+    public float scrollSpeed = -1.5f;
+    private int score;
+    public Text scoreText;
+
+    private void Awake()
+    {
+        if (GameController.instance == null)
+        {
+            GameController.instance = this;
+        }
+        else if (GameController.instance != this)
+        {
+            Destroy(gameObject);
+            Debug.LogWarning("GameController ha sido instanciado por segunda vez. Esto no deberia pasar");
+        }
+
+
+    }
+    // Start is called before the first frame update
+    public void BirdScore()
+    {
+        if (gameOver) return;
+
+        score++;
+        scoreText.text = "Score: " + score;
+        SoundSystem.instance.PlayCoin();
+    }
+
+
+    public void BirdDie()
+    {
+        gamerOverText.SetActive(true);
+        gameOver = true;
+
+    }
+    private void OnDestroy()
+    {
+        if (GameController.instance == this)
+        {
+            GameController.instance = null;
+        }
+
+
+    }
+}
+
